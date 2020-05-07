ffcc34cc30a09b58f9b4e0ac63fb1497df6acb48
Leonardo Gomes <leonardodasigomes@gmail.com>
2020-05-07 16:33:31 -0300

    Create react component class

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -1,84 +1,51 @@
-console.log('App is running!');
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault() 
-    const option = e.target.elements.option.value
-    if (option) {
-        app.options.push(option)
-        e.target.elements.option.value = ''
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indicision</h1>
+                <h2>Put your life in tha hands of a computer</h2>
+            </div>
+        );
     }
-    else {
+}
 
+class Action extends React.Component{
+    render () {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
     }
-    render()
 }
 
-const onRemoveAll = () => {
-    app.options = []
-    render()
+class Options extends React.Component {
+    render () {
+        return (
+          <div>
+              Options component here
+          </div>  
+        )
+    }
 }
 
-
-// let count = 0
-// const addOne = () => {
-//     count++
-//     renderCounterApp()
-// }
-// const minusOne = () => {
-//     count--
-//     renderCounterApp()
-// }
-// const reset = () => {
-//     count = 0
-//     renderCounterApp()
-// }
-
-
-
-// const templateTwo = (
-//     <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={addOne}>+1</button>
-//         <button onClick={minusOne}>-1</button>
-//         <button onClick={reset}>reset</button>
-//     </div>
-// )
-    
-const appRoot = document.getElementById('app')
-
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length)
-    const option = app.options[randomNum]
-    alert(option)
+class AddOption extends React.Component {
+    render () {
+        return (
+            <div>
+                LeoLeo
+            </div>
+        )
+    }
 }
 
-const render= () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                {
-                    app.options.map((option, id) => <li key={id}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type='text' name='option'/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    )
+const jsx = (
+    <div>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+    </div>
+)
 
-    ReactDOM.render(template, appRoot)
-}
-
-render()
+ReactDOM.render(jsx, document.getElementById('app'))
