a7f1c75009cd007603e19d07642404cbe4886c27
Leonardo Gomes <leonardodasigomes@gmail.com>
2020-08-28 00:45:53 -0300

    Add axios, font-awesome, moment and adjust the style aplication with all interaction

--- projectTwo/moduletwo/src/pages/Main/index.js
+++ projectTwo/moduletwo/src/pages/Main/index.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import moment from "moment";
 import logo from "../../assets/logo.png";
 import { Container, Form } from "./styles";
 import CompareList from "../../components/CompareList";
+import api from "../../services/api";
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: "",
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">Ok</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+  handleAddRepository = async (e) => {
+    e.preventDefault();
 
-export default Main;
+    this.setState({ loading: true });
+
+    try {
+      const { data: repository } = await api.get(
+        `/repos/${this.state.repositoryInput}`
+      );
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: "",
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form
+          withError={this.state.repositoryError}
+          onSubmit={this.handleAddRepository}
+        >
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={(e) => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">
+            {this.state.loading ? (
+              <i className="fa fa-spinner fa-pulse" />
+            ) : (
+              "Ok"
+            )}
+          </button>
+        </Form>
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
