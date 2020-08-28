b66c65247fa08066999ecb0677a6e2064ec1ac1c
Leonardo Gomes <leonardodasigomes@gmail.com>
2020-08-28 00:50:48 -0300

    Adjust folders in projectTwo

---
+++ projectTwo/build/precache-manifest.79eb7c37dfdd3505bece291958da46bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823435a8b3ec48969a07c9d77fb323e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3908e5272001ba53d976",
+    "url": "/static/js/2.7dd78523.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7dd78523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158ad32431cff4108c78",
+    "url": "/static/js/main.43d1db23.chunk.js"
+  },
+  {
+    "revision": "5afb08a979d0bfbb805b",
+    "url": "/static/js/runtime-main.1a2cbb59.js"
+  }
+]);
