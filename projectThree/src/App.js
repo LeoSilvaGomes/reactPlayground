88bc6955e7b811547f6184086cd3dbe11cd5d1f8
Leonardo Gomes <leonardodasigomes@gmail.com>
2020-09-02 01:27:10 -0300

    Add reactotron and create redux to the new project function

--- projectThree/src/App.js
+++ projectThree/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import "./config/ReactotronConfig";
 import { Provider } from "react-redux";
 
 import store from "./store";
 
-import TodoList from "./TodoList";
+import Routes from "./routes";
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
---
+++ projectThree/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Footer = ({ count }) => <p>Você tem {count} repositórios favoritos</p>;
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+const mapStateProps = (state) => ({
+  count: state.favorites.length,
+});
+
+export default connect(mapStateProps)(Footer);
